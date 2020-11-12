305437c5b239fc603bd36744fb9d2bb596561ff4
tomek-skrond <tomek2.skrond@gmail.com>
2020-11-12 21:22:46 +0100

    test

---
+++ bot.js
@@ -0,0 +1,22 @@
+console.log('hello bot');
+var fs = require('fs');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login(readDiscordKey);
+
+client.on('ready', readyDiscord);
+
+function readyDiscord(){
+    console.log('siusior');
+}
+
+function readDiscordKey(){
+    try {  
+        var data = fs.readFileSync('discord_auth.txt', 'utf8');
+        return data.toString();
+    } catch(e) {
+        console.log('Error:', e.stack);
+        return "error"
+    }
+}
